f851c8d6cac68abc3b6ed7ff614c60834435c274
Toti Carva <9rw85ptzh8@privaterelay.appleid.com>
2026-09-22 15:55:26 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,53 +1,5 @@
-const CACHE="vig-v2.1";
-const FILES=["./","./index.html","./manifest.webmanifest"];
-
-self.addEventListener("install",e=>{
-  self.skipWaiting();
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(FILES))
-  );
-});
-
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(
-        keys
-          .filter(key=>key!==CACHE)
-          .map(key=>caches.delete(key))
-      ))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-
-  if(e.request.mode==="navigate"){
-    e.respondWith(
-      fetch(e.request)
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>
-            cache.put("./index.html",copy)
-          );
-          return response;
-        })
-        .catch(()=>caches.match("./index.html"))
-    );
-    return;
-  }
-
-  e.respondWith(
-    caches.match(e.request).then(cached=>
-      cached ||
-      fetch(e.request).then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>
-          cache.put(e.request,copy)
-        );
-        return response;
-      })
-    )
-  );
-});
+const CACHE="vig-v2.2";
+const FILES=["./","./index.html","./manifest.webmanifest","./apple-touch-icon.png","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;if(e.request.mode==="navigate"){e.respondWith(fetch(e.request).then(r=>{const x=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",x));return r}).catch(()=>caches.match("./index.html")));return}e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{const x=n.clone();caches.open(CACHE).then(c=>c.put(e.request,x));return n})))});
